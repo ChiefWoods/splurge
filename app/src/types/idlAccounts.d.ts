a112b8a53869ac011fac906d0dc4ebb69f175eb1
ChiefWoods <chii.yuen@hotmail.com>
2025-01-03 01:37:07 +0800

    Add idlAccounts.d.ts

---
+++ app/src/types/idlAccounts.d.ts
@@ -0,0 +1,16 @@
+import { IdlAccounts } from '@coral-xyz/anchor';
+import { Splurge } from './splurge';
+
+export type SplurgeConfig = IdlAccounts<Splurge>['splurgeConfig'];
+export type Shopper = IdlAccounts<Splurge>['shopper'];
+export type Store = IdlAccounts<Splurge>['store'];
+export type StoreItem = IdlAccounts<Splurge>['storeItem'];
+export type Order = IdlAccounts<Splurge>['order'];
+export type Review = IdlAccounts<Splurge>['review'];
+
+export enum OrderStatus {
+  'pending',
+  'shipping',
+  'cancelled',
+  'completed',
+}
