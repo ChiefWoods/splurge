7be7ac97bd186f230f172b11a63476b7429f2b73
ChiefWoods <chii.yuen@hotmail.com>
2025-01-05 01:24:04 +0800

    Add 404 page

---
+++ app/src/app/not-found.tsx
@@ -0,0 +1,20 @@
+'use client';
+
+import { Button } from '@/components/ui/button';
+import { ArrowLeft } from 'lucide-react';
+import { useRouter } from 'next/navigation';
+
+export default function NotFound() {
+  const router = useRouter();
+
+  return (
+    <section className="main-section">
+      <h2>404</h2>
+      <p>Page not found.</p>
+      <Button onClick={router.back}>
+        <ArrowLeft />
+        Go Back
+      </Button>
+    </section>
+  );
+}
