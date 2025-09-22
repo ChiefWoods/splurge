b1be858871ccf1e5fd0a6cb0111e970ed3d786d0
Chii Yuen <chii.yuen@hotmail.com>
2025-09-22 20:05:05 +0800

    Add OrderTable pagination

--- app/src/components/OrderTable.tsx
+++ app/src/components/OrderTable.tsx
@@ -8,6 +8,7 @@ import {
   flexRender,
   getCoreRowModel,
   getFilteredRowModel,
+  getPaginationRowModel,
   getSortedRowModel,
   useReactTable,
 } from '@tanstack/react-table';
@@ -22,7 +23,15 @@ import {
   TableRow,
 } from './ui/table';
 import { Button } from './ui/button';
-import { ArrowUpDown, ArrowUp, ArrowDown } from 'lucide-react';
+import {
+  ArrowUpDown,
+  ArrowUp,
+  ArrowDown,
+  ChevronsLeft,
+  ChevronLeft,
+  ChevronRight,
+  ChevronsRight,
+} from 'lucide-react';
 import { capitalizeFirstLetter, atomicToUsd } from '@/lib/utils';
 import { ParsedItem, ParsedOrder } from '@/types/accounts';
 import { InfoTooltip } from './InfoTooltip';
@@ -32,6 +41,13 @@ import Link from 'next/link';
 import { getAccountLink } from '@/lib/solana-client';
 import { SquareArrowOutUpRight } from 'lucide-react';
 import { ACCEPTED_MINTS_METADATA } from '@/lib/constants';
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from './ui/select';
 
 const ORDER_TABS = ['all', 'pending', 'shipping', 'completed', 'cancelled'];
 
@@ -259,6 +275,7 @@ export function OrderTable({
     getCoreRowModel: getCoreRowModel(),
     getSortedRowModel: getSortedRowModel(),
     getFilteredRowModel: getFilteredRowModel(),
+    getPaginationRowModel: getPaginationRowModel(),
     state: {
       sorting,
       columnFilters,
@@ -343,10 +360,81 @@ export function OrderTable({
           </TableBody>
         </Table>
       </div>
-
-      <p className="muted-text text-sm">
-        {table.getRowModel().rows.length} item(s) found.
-      </p>
+      <div className="flex items-center justify-between px-2">
+        <p className="text-sm text-muted-foreground">
+          {table.getRowCount()} item(s) found.
+        </p>
+        <div className="flex items-center space-x-6 lg:space-x-8">
+          <div className="flex items-center space-x-2">
+            <p className="text-sm font-medium">Rows per page</p>
+            <Select
+              value={`${table.getState().pagination.pageSize}`}
+              onValueChange={(value) => {
+                table.setPageSize(Number(value));
+              }}
+            >
+              <SelectTrigger className="h-8 w-[70px]">
+                <SelectValue
+                  placeholder={table.getState().pagination.pageSize}
+                />
+              </SelectTrigger>
+              <SelectContent side="top">
+                {[10, 20, 30, 40, 50].map((pageSize) => (
+                  <SelectItem key={pageSize} value={`${pageSize}`}>
+                    {pageSize}
+                  </SelectItem>
+                ))}
+              </SelectContent>
+            </Select>
+          </div>
+          <div className="flex w-[100px] items-center justify-center text-sm font-medium">
+            Page {table.getState().pagination.pageIndex + 1} of{' '}
+            {table.getPageCount()}
+          </div>
+          <div className="flex items-center space-x-2">
+            <Button
+              variant="outline"
+              size="icon"
+              className="hidden size-8 lg:flex"
+              onClick={() => table.setPageIndex(0)}
+              disabled={!table.getCanPreviousPage()}
+            >
+              <span className="sr-only">Go to first page</span>
+              <ChevronsLeft />
+            </Button>
+            <Button
+              variant="outline"
+              size="icon"
+              className="size-8"
+              onClick={() => table.previousPage()}
+              disabled={!table.getCanPreviousPage()}
+            >
+              <span className="sr-only">Go to previous page</span>
+              <ChevronLeft />
+            </Button>
+            <Button
+              variant="outline"
+              size="icon"
+              className="size-8"
+              onClick={() => table.nextPage()}
+              disabled={!table.getCanNextPage()}
+            >
+              <span className="sr-only">Go to next page</span>
+              <ChevronRight />
+            </Button>
+            <Button
+              variant="outline"
+              size="icon"
+              className="hidden size-8 lg:flex"
+              onClick={() => table.setPageIndex(table.getPageCount() - 1)}
+              disabled={!table.getCanNextPage()}
+            >
+              <span className="sr-only">Go to last page</span>
+              <ChevronsRight />
+            </Button>
+          </div>
+        </div>
+      </div>
     </Tabs>
   );
 }
