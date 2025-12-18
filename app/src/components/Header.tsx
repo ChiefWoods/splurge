d87ab725f610b8b3fda82bd853e42f85c03aa4c3
Chii Yuen <chii.yuen@hotmail.com>
2025-12-18 18:26:07 +0800

    Add sheet navbar

--- app/src/components/Header.tsx
+++ app/src/components/Header.tsx
@@ -1,21 +1,30 @@
 'use client';
 
-import { ShoppingCartIcon } from 'lucide-react';
+import { Menu, ShoppingCartIcon } from 'lucide-react';
 import Link from 'next/link';
 import { Avatar, AvatarImage } from './ui/avatar';
 import { useShopper } from '@/providers/ShopperProvider';
 import { usePersonalStore } from '@/providers/PersonalStoreProvider';
-import { useMemo } from 'react';
+import { useMemo, useState } from 'react';
 import { Keypair } from '@solana/web3.js';
 import { DialectNotification } from './DialectNotification';
 import { UnifiedWalletButton, useUnifiedWallet } from '@jup-ag/wallet-adapter';
 import { DicebearStyles, getDicebearEndpoint } from '@/lib/client/dicebear';
 import { SettingsDropdown } from './SettingsDropdown';
+import {
+  Sheet,
+  SheetContent,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from './ui/sheet';
+import { Button } from './ui/button';
 
 export function Header() {
   const { publicKey } = useUnifiedWallet();
   const { shopperData } = useShopper();
   const { personalStoreData } = usePersonalStore();
+  const [isOpen, setIsOpen] = useState(false);
 
   const avatarSeed = useMemo(() => {
     return publicKey?.toBase58() ?? Keypair.generate().publicKey.toBase58();
@@ -35,16 +44,21 @@ export function Header() {
   ];
 
   return (
-    <header className="border-b-primary bg-background sticky top-0 z-10 flex h-20 items-center justify-between border-b px-6 py-4">
+    <header className="border-b-primary bg-background sticky top-0 z-10 flex h-20 items-center justify-between gap-4 border-b px-6 py-4">
       <Link href={'/'} className="*:text-primary flex items-center gap-2">
         <ShoppingCartIcon size={24} />
-        <h1 className="text-3xl font-medium">Splurge</h1>
+        <h1 className="hidden text-2xl font-medium sm:block md:text-3xl">
+          Splurge
+        </h1>
       </Link>
-      <nav className="flex items-center gap-6">
+      <nav className="ml-auto hidden items-center gap-6 md:flex">
         <ul className="flex items-center gap-4">
           {navLinks.map(({ name, href }) => (
             <li key={href}>
-              <Link href={href} className="font-medium hover:underline">
+              <Link
+                href={href}
+                className="font-medium text-nowrap hover:underline"
+              >
                 {name}
               </Link>
             </li>
@@ -54,26 +68,70 @@ export function Header() {
           <DialectNotification />
           <SettingsDropdown />
         </div>
-        <div className="flex items-center gap-3">
-          {shopperData && (
-            <Link href={`/shoppers/${shopperData.publicKey}`}>
-              <Avatar className="size-7">
-                <AvatarImage
-                  src={
-                    shopperData.image ??
-                    getDicebearEndpoint(DicebearStyles.Shopper, avatarSeed)
-                  }
-                  className="bg-white"
-                />
-              </Avatar>
-            </Link>
-          )}
-          <UnifiedWalletButton
-            buttonClassName="bg-primary!"
-            currentUserClassName="bg-primary!"
-          />
-        </div>
       </nav>
+      <div className="ml-auto flex gap-1 md:hidden">
+        <div className="hidden gap-1 sm:flex">
+          <DialectNotification />
+          <SettingsDropdown />
+        </div>
+        <Sheet open={isOpen} onOpenChange={setIsOpen}>
+          <SheetTrigger>
+            <Button
+              variant="ghost"
+              size={'icon'}
+              className="*:text-foreground size-8"
+            >
+              <Menu />
+            </Button>
+          </SheetTrigger>
+          <SheetContent>
+            <SheetHeader>
+              <Link
+                href={'/'}
+                className="*:text-primary flex items-center gap-2"
+                onClick={() => setIsOpen(false)}
+              >
+                <ShoppingCartIcon size={20} />
+                <SheetTitle className="text-xl font-medium">Splurge</SheetTitle>
+              </Link>
+            </SheetHeader>
+            <nav className="mt-6">
+              <ul className="flex flex-col gap-4">
+                {navLinks.map(({ name, href }) => (
+                  <li key={href}>
+                    <Link
+                      href={href}
+                      className="font-medium text-nowrap hover:underline"
+                      onClick={() => setIsOpen(false)}
+                    >
+                      {name}
+                    </Link>
+                  </li>
+                ))}
+              </ul>
+            </nav>
+          </SheetContent>
+        </Sheet>
+      </div>
+      <div className="flex items-center gap-3">
+        {shopperData && (
+          <Link href={`/shoppers/${shopperData.publicKey}`}>
+            <Avatar className="size-7">
+              <AvatarImage
+                src={
+                  shopperData.image ??
+                  getDicebearEndpoint(DicebearStyles.Shopper, avatarSeed)
+                }
+                className="bg-white"
+              />
+            </Avatar>
+          </Link>
+        )}
+        <UnifiedWalletButton
+          buttonClassName="bg-primary!"
+          currentUserClassName="bg-primary!"
+        />
+      </div>
     </header>
   );
 }
